b672a8c90be15891e2de37788e62ffd3e6d19f25
marluxdxd <marluxdxd@gmail.com>
2025-10-17 09:48:45 +0800

    convert to statefulwidget

--- lib/bottom_nav_bar.dart
+++ lib/bottom_nav_bar.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
 
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
 
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
 
-
+class _MyWidgetState extends State<MyWidget> {
+  int _selectedIndex = 4; // start with 'redeem' selected (index 2)
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
 
 
 class ButtomView extends StatelessWidget {
